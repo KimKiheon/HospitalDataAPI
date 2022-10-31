fbb60094dc63affb93e775cd404df38c08394e15
Kimkiheon <cbj2741@gmail.com>
2022-10-31 11:23:03 +0900

    Initial commit

---
+++ src/main/java/com/example/hospitaldata/HospitalDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitaldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalDataApplication.class, args);
+	}
+
+}
