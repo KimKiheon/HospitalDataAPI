066b986459f8d4e081f6ea63fbdd78ceefffcd66
Kimkiheon <cbj2741@gmail.com>
2022-10-31 15:40:09 +0900

    Test Success

--- src/main/java/com/example/hospitaldata/parser/ReadLineContext.java
+++ src/main/java/com/example/hospitaldata/parser/ReadLineContext.java
@@ -26,7 +26,7 @@ public class ReadLineContext<T> {
             try {
                 result.add(parser.parse(str));
             } catch (Exception e) {
-                System.out.printf("파싱중 문제가 생겨 이 라인은 넘어갑니다. 파일내용:%s", str);
+                System.out.printf("파싱중 문제가 생겨 이 라인은 넘어갑니다. 파일내용:%s\n", str);
             }
         }
         reader.close();
