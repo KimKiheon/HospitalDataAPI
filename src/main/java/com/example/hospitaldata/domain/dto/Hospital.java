7a40540bfbc7a2f2c9a9cacbde54919ebc65234e
Kimkiheon <cbj2741@gmail.com>
2022-10-31 13:15:18 +0900

    Hospital Domain

---
+++ src/main/java/com/example/hospitaldata/domain/dto/Hospital.java
@@ -0,0 +1,30 @@
+package com.example.hospitaldata.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class Hospital {
+    private int id;
+    private String openServiceName;
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;;
+    private int businessStatusCode;
+    private int phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+
+
+}
