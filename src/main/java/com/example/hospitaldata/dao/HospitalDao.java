fd8049216c280f7ea6b1c3d8491ae3309cd37874
Kimkiheon <cbj2741@gmail.com>
2022-11-01 15:12:23 +0900

    findById

--- src/main/java/com/example/hospitaldata/dao/HospitalDao.java
+++ src/main/java/com/example/hospitaldata/dao/HospitalDao.java
@@ -14,6 +14,23 @@ public class HospitalDao {
     public HospitalDao(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
+    RowMapper<Hospital> rowMapper = (rs, rowNum) ->
+            new Hospital(Integer.valueOf(rs.getString("id")),
+                    rs.getString("open_service_name"),
+                    Integer.valueOf(rs.getString("open_local_government_code")),
+                    rs.getString("management_number"),
+                    rs.getTimestamp("license_date").toLocalDateTime(),
+                    Integer.valueOf(rs.getString("business_status")),
+                    Integer.valueOf(rs.getString("business_status_code")),
+                    rs.getString("phone"),
+                    rs.getString("full_address"),
+                    rs.getString("road_name_address"),
+                    rs.getString("hospital_name"),
+                    rs.getString("business_type_name"),
+                    Integer.valueOf(rs.getString("healthcare_provider_count")),
+                    Integer.valueOf(rs.getString("patient_room_count")),
+                    Integer.valueOf(rs.getString("total_number_of_beds")),
+                    Float.valueOf(rs.getString("total_area_size")));
 
     public void add(Hospital hospital){
         String sql = "INSERT INTO hospitals (id, open_service_name, open_local_government_code, management_number, license_date, business_status, business_status_code, phone, full_address, road_name_address, hospital_name, business_type_name, healthcare_provider_count, patient_room_count, total_number_of_beds, total_area_size)" +
@@ -33,6 +50,9 @@ public class HospitalDao {
     }
     public int getCount(){
         return this.jdbcTemplate.queryForObject("select count(*) from hospitals;", Integer.class);
+    }
+    public Hospital findById(int id){
+        return jdbcTemplate.queryForObject("select *from hospitals where id = ?",rowMapper,id);
     }
 
 
