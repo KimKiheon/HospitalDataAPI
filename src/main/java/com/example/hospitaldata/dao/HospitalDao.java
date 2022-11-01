ee0a772f6db080ec07fe6bb77e22418768ba4d53
Kimkiheon <cbj2741@gmail.com>
2022-11-01 14:25:21 +0900

    Add Test Success

--- src/main/java/com/example/hospitaldata/dao/HospitalDao.java
+++ src/main/java/com/example/hospitaldata/dao/HospitalDao.java
@@ -16,14 +16,17 @@ public class HospitalDao {
     }
 
     public void add(Hospital hospital){
-        this.jdbcTemplate.update("insert into hospitals (id, open_service_name, open_local_government_code, " +
-                        "management_number, license_date, business_status, business_status_code, phone " +
-                        "full_address, road_name_address, hospital_name, business_type_name, healthcare_provider_count, " +
-                        "patient_room_count, total_number_of_beds, total_area_size)" +
-                "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);",hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
-                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),hospital.getBusinessStatusCode(),hospital.getPhone(),
-                hospital.getFullAddress(), hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(), hospital.getHealthcareProviderCount(),
-                hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize());
+        String sql = "INSERT INTO hospitals (id, open_service_name, open_local_government_code, management_number, license_date, business_status, business_status_code, phone, full_address, road_name_address, hospital_name, business_type_name, healthcare_provider_count, patient_room_count, total_number_of_beds, total_area_size)" +
+                " VALUES (?,?,?,?," +
+                " ?,?,?,?," +
+                " ?,?,?,?," +
+                " ?,?,?,?);"; // 16개
+        this.jdbcTemplate.update(sql,
+                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(), hospital.getManagementNumber(),
+                hospital.getLicenseDate(), hospital.getBusinessStatus(), hospital.getBusinessStatusCode(), hospital.getPhone(),
+                hospital.getFullAddress(), hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
+                hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize()
+        );
     }
     public void deleteAll(){
         this.jdbcTemplate.update("delete from hospitals");
